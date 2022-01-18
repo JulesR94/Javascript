a1f24644990905e33aae54c4b5ce9c67df557245
JulesR94 <96482907+JulesR94@users.noreply.github.com>
2022-01-18 15:42:24 +0100

    Update main.js

--- test/main.js
+++ test/main.js
@@ -13,13 +13,43 @@
 //alert(number_2); // Affiche : « 1 
 
 
-let number = 0;
-let output = number++;
-document.write(number); // Affiche : « 1 » 
-document.write(output); // Affiche : « 0 »
+//let number = 0;
+//let output = number++;
+//document.write(number); // Affiche : « 1 » 
+//document.write(output); // Affiche : « 0 »
 
-let number = 0;
-let output = ++number;
-document.write(number); // Affiche : « 1 » 
-document.write(output);
+//let number = 0;
+//let output = ++number;
+//document.write(number); // Affiche : « 1 » 
+//document.write(output);
+
+//let number = 1;
+//while (number < 10) {number++; } //incrémentation
+//alert(number); // Affiche : « 10 »
+
+//for(let iter = 0; iter < 10 ;iter++) {
+//    alert("itération n° "+ iter)
+//}
+
+//function showMsg() {
+//    alert("C'est une premiére fonction");
+// }  
+
+//let var1 = 2, var2= 3;
+//function calculate(){
+//    alert(var1 * var2);
+//}
+//calculate();
+//alert(var1 * var2);
+
+let var1 = 2, var2 = 3;
+function calculate() {
+    alert(var1 * var2);
+}
+
+calculate();
+alert(var1);
+var1 = 3;
+calculate();
+alert(var1);
 
