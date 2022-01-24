14f54058a58199982b59b64f46ab0c77f6c4b993
JulesR94 <96482907+JulesR94@users.noreply.github.com>
2022-01-24 10:58:10 +0100

    Update main2.js

--- exercices/main2.js
+++ exercices/main2.js
@@ -1,9 +1,8 @@
-/* let chiffre1, chiffre2;
+let chiffre1, chiffre2;
 chiffre1 = parseInt(prompt("votre premier nombre : "));
 chiffre2 = parseInt(prompt("votre deuxiéme nombre"));
 let calculate2 = chiffre1 + chiffre2;
-alert(calculate2); */
-
+alert(calculate2); 
 
 
 
