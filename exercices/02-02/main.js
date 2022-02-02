4dcda8a6316ab85e168d2279b8409518e116fcda
JulesR94 <96482907+JulesR94@users.noreply.github.com>
2022-02-02 15:29:38 +0100

    exo

---
+++ exercices/02-02/main.js
@@ -0,0 +1,15 @@
+// let nb1 = "2";
+// let nb2 = "0";
+// let nb3 = "1";
+// let nb4 = "6";
+
+// let ordre = [nb1,nb2,nb2,nb4];
+// alert(ordre);
+
+// let ordreinv = [nb4,nb3,nb2,nb1];
+// alert(ordreinv);
+let chiffres = ['2','0','1','6'];
+alert(chiffres[0]+chiffres[1]+chiffres[2]+chiffres[3]);
+
+ordreinv = chiffres[3]+chiffres[2]+chiffres[1]+chiffres[0];
+alert(ordreinv);
